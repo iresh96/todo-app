c2b97b3502e6a531a60b7edfb4eb3dfdb6402de1
Iresh Madushanka <ireshmadushanka03@gmail.com>
2021-03-07 23:10:13 +0530

    filter by priority fixed

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -20,34 +20,27 @@ const ToDo = ({ todos, completeTodo, removeTodo, updateTodo, group }) => {
     });
   };
 
-  const compare = (a, b) => {
-    if (group === "Color") {
-      if (a.color < b.color) {
-        return -1;
-      }
-      if (a.color > b.color) {
-        return 1;
-      }
-      return 0;
+  const compareColor = (a, b) => {
+    if (a.color < b.color) {
+      return -1;
     }
-    if (group === "Priority") {
-      if (a.priority < b.priority) {
-        return -1;
-      }
-      if (a.priority > b.priority) {
-        return 1;
-      }
-      return 0;
+    if (a.color > b.color) {
+      return 1;
     }
+    return 0;
   };
 
-  if (edit.id) {
-    return <ToDoModal edit={edit} onSubmit={submitUpdate} />;
-  }
-
-  if (group !== "") {
-    const sortedlist = todos.sort(compare);
-    return sortedlist.map((todo, index) => (
+  const comparePriority = (a, b) => {
+    if (a.temp < b.temp) {
+      return -1;
+    }
+    if (a.temp > b.temp) {
+      return 1;
+    }
+    return 0;
+  };
+  const uigenerator = (array) => {
+    return array.map((todo, index) => (
       <div
         className={
           todo.isComplete
@@ -77,37 +70,28 @@ const ToDo = ({ todos, completeTodo, removeTodo, updateTodo, group }) => {
         </div>
       </div>
     ));
+  };
+
+  if (edit.id) {
+    return <ToDoModal edit={edit} onSubmit={submitUpdate} />;
   }
-  return todos.map((todo, index) => (
-    <div
-      className={
-        todo.isComplete
-          ? `todo-row complete ${todo.color}`
-          : `todo-row ${todo.color}`
-      }
-      key={index}
-    >
-      <Checkbox
-        checked={todo.isComplete}
-        onClick={() => completeTodo(todo.id)}
-        color="default"
-      />
-      <Chip ize="small" label={todo.priority} />
-      <div key={todo.id} className="text">
-        {todo.text}{" "}
-      </div>
-      <div className="icons">
-        <RiCloseCircleLine
-          onClick={() => removeTodo(todo.id)}
-          className="delete-icon"
-        />
-        <TiEdit
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
-          className="edit-icon"
-        />
-      </div>
-    </div>
-  ));
+
+  if (group === "Color") {
+    const sortedlist = todos.sort(compareColor);
+    return uigenerator(sortedlist);
+  }
+
+  if (group === "Priority") {
+    const newTodos = todos.map((todo) => {
+      if (todo.priority === "Low") todo.temp = 3;
+      if (todo.priority === "Medium") todo.temp = 2;
+      if (todo.priority === "High") todo.temp = 1;
+      return todo;
+    });
+
+    return uigenerator(newTodos.sort((a, b) => a.temp - b.temp));
+  }
+  return uigenerator(todos);
 };
 
 export default ToDo;
